9960ec0c7b5117f5ddd6ceaa6c5d1c85f6cba571
Tim Mironov <timirlan666@gmail.com>
2025-10-13 18:59:39 +0500

    + simple service

--- apps/service/src/external_ip_service/__init__.py
+++ apps/service/src/external_ip_service/__init__.py
@@ -1,2 +1,21 @@
+from redis import Redis
+from time import sleep, time
+from loguru import logger
+# from sys import stdout
+
+
 def main() -> None:
-    print("Hello from external-ip-service!")
+    # logger.add(stdout, format="{time} [{level}] {message}", level="INFO")
+
+    logger.info("Connecting redis server...")
+    redis = Redis()
+
+    logger.info("Start service")
+    while True:
+        try:
+            redis.set("time", str(int(time())))
+            logger.info("Time was updated")
+            sleep(10)
+        except KeyboardInterrupt:
+            logger.info("Exit")
+            return 0
