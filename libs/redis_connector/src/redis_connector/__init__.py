26bf64dbc3f68d34b128a353076fad1124e2f229
Tim Mironov <timirlan666@gmail.com>
2025-10-13 20:34:14 +0500

    ! ruff in connector

--- libs/redis_connector/src/redis_connector/__init__.py
+++ libs/redis_connector/src/redis_connector/__init__.py
@@ -1,6 +1,7 @@
+from json import dumps, loads
 from typing import Optional, Union
+
 from redis import Redis
-from json import loads, dumps
 
 __all__ = ["RedisConnector"]
 
@@ -8,14 +9,14 @@ IPInfo = Union[object, str]
 
 
 class RedisConnector:
-    def __init__(self, redis_key: str = "external_ip_info"):
+    def __init__(self, redis_key: str = "external_ip_info") -> None:
         self.connection: Optional[Redis] = None
         self.key = redis_key
 
-    def connect(self):
+    def connect(self) -> None:
         self.connection = Redis()
 
-    def setIpInfo(self, ip_information: Optional[IPInfo]):
+    def setIpInfo(self, ip_information: Optional[IPInfo]) -> None:
         self.connection.set(self.key, dumps(ip_information))
 
     def getIpInfo(self) -> Optional[IPInfo]:
